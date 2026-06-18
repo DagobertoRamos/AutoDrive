92a835aa170a1405bf5dea7b3c1b9019fdb6788b
Dagoberto Ramos <beto1910@gmail.com>
2026-06-18 19:00:15 -0300

    fix: adiciona configuracao de memoria do build Next

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+
+  experimental: {
+    webpackMemoryOptimizations: true,
+    webpackBuildWorker: true,
+    serverSourceMaps: false,
+    preloadEntriesOnStart: false,
+  },
+}
+
+export default nextConfig
